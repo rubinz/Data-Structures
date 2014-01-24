1c42b4d85d00fc0678ffadcf7fd1f5fca7725de2
Zevi <zevirc@gmail.com>
2014-01-24 17:01:02 -0500

    Lab 1 work and tests

---
+++ Lab1Part2.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	short x, y, sum;
+
+	cout << "Enter X: ";
+	cin >> x;
+
+	cout << "Enter Y:";
+	cin >> y;
+
+	cout << endl << "You entered " << x << " and " << y << endl;
+
+	sum = x + y;
+
+	cout << "Sum is: " << sum << endl;
+	// between 16790 and 16989
+}
---
+++ helloworldtest.cpp
@@ -0,0 +1,28 @@
+#include "string.h" 
+#include <iostream>
+
+using namespace std;
+int main()
+{
+	string stringMom = "Data Structures" ;
+	char x = 'M';
+	bool isDone = true;
+	short n = 14;
+	int I = 7;
+	long y = 314159265358979;
+	float pi = 3.1415;
+	double Y = .31415;
+
+	cout << '\n' << x << ' ' << isDone << ' ' << n << ' ' << I << ' ' << y << ' ' << pi << ' ' << Y << ' ' << stringMom << endl;
+
+
+	cout << "\nHello World" << endl;
+	
+	//cout << "\t\t\t like" << endl;
+	//cout << "\t\t flies \t\t an" << endl;
+	//cout << "your health \t\t\t\t arrow ... ping!" << endl;
+	
+
+	return 0;
+}
+
